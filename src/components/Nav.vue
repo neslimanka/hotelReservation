cc61b4b66c9ecef58368792785e6f423ac27ed7b
neslimanka <nesli.11.67@gmail.com>
2023-05-12 14:20:49 +0300

    router-link eklendi

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,15 +3,17 @@
     <div class="flex flex-col bg-cyan-950">
       <div class="flex md:flex-row flex-col justify-between items-center m-6">
         <h1 class="flex text-white md:text-2xl text-base font-semibold md:mb-0 mb-3">Otel Rezervasyon Sistemi</h1>
-        <button class="rounded-md bg-white text-black font-semibold md:p-3 p-2 md:text-lg text-sm">Yeni Rezervasyon Yap</button>
+        <router-link to="/" class="rounded-md bg-white text-black font-semibold md:p-3 p-2 md:text-lg text-sm">
+          Yeni Rezervasyon Yap
+        </router-link>
       </div>
     </div>
     <div class="container m-auto">
-      <div class="flex flex-row bg-stone-300 rounded-lg my-12 justify-around">
+      <div class="flex flex-row bg-stone-200 rounded-lg my-12 justify-around">
         <div
           v-for="(item,index) in data" :key="index"
           class="flex flex-col items-center justify-center my-12">
-          <div class="flex items-center justify-center rounded-full md:w-24 md:h-24 border border-1">
+          <div class="flex items-center justify-center rounded-full md:w-24 md:h-24 border border-1 border-grey-200">
             <img class="w-1/2" :src="item.image" alt="Calendar" />
           </div>
             <span class="text-base font-semibold text-center items-center justify-center">{{ item.text }}</span>
--- src/views/Landspace.vue
+++ src/views/Landspace.vue
@@ -1,23 +1,67 @@
 <template>
-   <div>
-    <p>girisTarihi: {{ this.formData.gidisTarihi }}</p>
-    <p>yetiskinSayisi: {{ this.formData.yetiskinSayisi }}</p>
-    <p>cocukSayisi: {{ this.formData.cocukSayisi }}</p>
-    <p>selectedValue: {{ this.formData.selectedValue }}</p>
+  <div class="container m-auto">
+    <div class="p-6 rounded-lg border border-1 bg-stone-200 leading-8">
+      <h1 class="font-semibold text-2xl">Otel İsmi</h1>
+      <span class="font-semibold">Giriş Tarihi: <b class="font-normal">{{formData.girisTarihi}} - </b></span>
+      <span class="font-semibold">Gidiş Tarihi: <b class="font-normal">{{formData.gidisTarihi}} - </b></span>
+      <span class="font-semibold">Yetişkin: <b class="font-normal">{{formData.yetiskinSayisi}} - </b></span>
+      <span class="font-semibold">Çocuk: <b class="font-normal">{{formData.cocukSayisi}} </b></span>
+    </div>
+    <div class="py-6">
+      <h1 class="font-semibold text-xl">Oda Tipi Seçimi</h1>
+      <button @click="fetchAPIData">TıkTıkTık</button>
+      <div 
+        class="flex flex-row "
+        v-for="item in data"
+        :key="item.hotel_id"
+      >
+        <div class="flex flex-col w-1/3">
+          <span>Standart</span>
+          <img class="w-1/4" :src="item.image"/>
+        </div>
+        
+      </div>
+    </div>
   </div>
 </template>
   
 <script>
 import { mapState } from 'vuex';
+import axios from 'axios';
+
+
 
 export default {
   name: 'Landspace',
 
+  data() {
+    return {
+      data: []
+    }
+  },
+
+  methods: {
+    fetchAPIData() {
+      console.log(this.data , "nesloş")
+    }
+  },
   computed: {
     ...mapState({
       formData: 'formData'
     }),
   },
+  mounted() {
+    axios.get('../../public/hotel.json')
+      .then(response => {
+        this.data = response.data;
+        console.log(this.data , "data")
+        console.log(response , "response")
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+  }
 };
 </script>
   
